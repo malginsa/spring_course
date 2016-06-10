815d55da59b35bdc9f59b019c34fc5cbf19526c8
Sergei Malgin <Sergei_Malgin@epam.com>
2016-06-10 10:13:14 +0300

    property

--- src/main/java/com/epam/spring/core/Client.java
+++ src/main/java/com/epam/spring/core/Client.java
@@ -4,6 +4,7 @@ public class Client {
 
 	private String id;
 	private String fullName;
+	private String greeting;
 
 	public Client(String id, String fullName) {
 		this.id = id;
@@ -18,5 +19,16 @@ public class Client {
 		return fullName;
 	}
 	
-	
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
 }
